a01706711ad3b0f6e46fb0bebc7f414fc4fa14ce
Danii44 <danishahmad444@gmail.com>
2025-05-16 12:18:47 +0400

    hh

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const rpcHost =
 
 export const candyMachineId = new PublicKey(
   process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
-    "5huM31dF47yS3JHpjFWgYj3q26iUDqM82oiE6BpCzinU"
+    "HgYU8kdBWSUcvc1DZH951H9KS5JAgEDn12qcVButrKHf"
 );
 
 export const defaultGuardGroup =
